de39e1652a22d5811f08eef400d41b4d08d25d21
Mohammadhasan <mohammadhasan.851385@gmail.com>
2026-02-12 12:19:19 +0330

    Add project structure and function skeleton

---
+++ main.py
@@ -0,0 +1,30 @@
+def fetch_books():
+    """
+    Fetch books data from OpenLibrary API.
+    """
+    pass
+
+
+def filter_books(books):
+    """
+    Filter books published after the year 2000.
+    """
+    pass
+
+
+def save_to_csv(books):
+    """
+    Save filtered books to a CSV file.
+    """
+    pass
+
+
+def main():
+    """
+    Main execution flow of the script.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
